d1c93db8ed5779bfc01775f7a3bb07b41348eadc
lucsaprigio <lucsaprigio@hotmail.com>
2022-11-02 10:33:59 -0300

    start server

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import Fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+// Mostrar logs no terminal
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    // criação de rotas
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+
+        return { count }
+    })
+
+    await fastify.listen({ port: 3333 })
+}
+
+bootstrap()
